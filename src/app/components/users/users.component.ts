dda5995cad18bbd3816f20eba4e4ac9dc32519f7
eyalmol <eyalmola18@gmail.com>
2021-12-21 12:27:52 +0200

    loops_with_ng_for

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,14 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-
+import { User } from '../../models/User';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css'],
 })
 export class UsersComponent implements OnInit {
+  users: User[] | any;
   constructor() {}
 
   ngOnInit(): void {
-    console.log('init...');
+    this.users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 30,
+        address: {
+          street: '50 Main st',
+          city: 'Boston',
+          state: 'MA',
+        },
+      },
+      {
+        firstName: 'Kevin',
+        lastName: 'Jonshon',
+        age: 34,
+        address: {
+          street: '20 schill st',
+          city: 'Lynn',
+          state: 'MA',
+        },
+      },
+      {
+        firstName: 'karen',
+        lastName: 'Williams',
+        age: 26,
+        address: {
+          street: '55 Mill st',
+          city: 'Miami',
+          state: 'Florida',
+        },
+      },
+    ];
+    this.addUser({
+      firstName: 'David',
+      lastName: 'Jackson',
+      age: 26,
+      address: {
+        street: '12 wake st',
+        city: 'Miami',
+        state: 'Florida',
+      },
+    });
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 }
